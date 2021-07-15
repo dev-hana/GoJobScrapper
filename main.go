e0f7c49f66d9e989c5c4cab597c61eeee22f079c
comghana <ji970322@naver.com>
2021-07-15 11:08:03 +0900

    create echo server

--- main.go
+++ main.go
@@ -1,7 +1,18 @@
 package main
 
-import "github.com/dev-hana/GoJobScrapper/scrapper"
+import (
+	"net/http"
+
+	"github.com/labstack/echo"
+)
+
+func handleHome(c echo.Context) error {
+	return c.String(http.StatusOK, "Hello World!")
+}
 
 func main() {
-	scrapper.Scrape("term")
+	e := echo.New()
+	e.GET("/", handleHome)
+	e.Logger.Fatal(e.Start(":1323"))
+
 }
